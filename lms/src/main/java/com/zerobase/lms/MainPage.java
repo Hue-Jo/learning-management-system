3323b40fcd1df33afe23520324ce0f488e236e12
HueJo <slack_water@naver.com>
2024-07-08 19:11:03 +0900

    write html using response object

---
+++ lms/src/main/java/com/zerobase/lms/MainPage.java
@@ -0,0 +1,42 @@
+package com.zerobase.lms;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+// 인터넷주소와 물리적인파일 매핑
+@Controller
+public class MainPage {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Index Page";
+    }
+
+    //request -> web -> server
+    //response -> server -> web
+    @RequestMapping("/hello")
+    public void hello(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.setContentType("text/html;charset=utf-8");
+
+        PrintWriter printWriter = response.getWriter();
+
+        String msg = "<html>"
+                + "<head>"
+                + "<meta charset=\"UTF-8\">"
+                + "</head>"
+                + "<body>"
+                + "<p> Hello World!</p> <p>This is zerobase website</p>"
+                + "<p> 안녕~! </p>"
+                + "</body>"
+                + "</html>";
+
+        printWriter.write(msg);
+        printWriter.close();
+    }
+}
